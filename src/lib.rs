411bef703535d8513965275e171e0ed82dcf1a68
mhg <mhg@urml.eu>
2016-10-01 22:30:11 +0100

    Export more symbols

--- src/lib.rs
+++ src/lib.rs
@@ -9,5 +9,5 @@ pub use tokenize::Tokenizer;
 pub use grammar::Grammar;
 pub use grammar::load_grammar_str;
 pub use compiler::{compile_grammar, compile_grammar_file};
-pub use vm::run;
-pub use htmltokenize::tokenize_html;
+pub use vm::{run, StreamingHandler};
+pub use htmltokenize::{tokenize_html, HTMLToken};
