d16755335600e10b7135500949ea504e6eb9dadf
mhg <mhg@urml.eu>
2016-10-24 21:26:09 +0100

    Export CompiledGrammar symbol

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,6 @@ mod tests;
 pub use tokenize::Tokenizer;
 pub use grammar::Grammar;
 pub use grammar::load_grammar_str;
-pub use compiler::{compile_grammar, compile_grammar_file};
+pub use compiler::{compile_grammar, compile_grammar_file, CompiledGrammar};
 pub use vm::{run, StreamingHandler};
 pub use htmltokenize::{tokenize_html, HTMLToken};
